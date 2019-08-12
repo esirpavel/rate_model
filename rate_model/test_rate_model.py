39186fb59261357ebef2f146684d027849bf752e
Pavel Esir (thinkpad) <esir.pavel@gmail.com>
2019-08-12 11:27:11 +0300

    Moved parameters dictionary to config.py file. Changed simulation time_step for burting mode in testing data

--- rate_model/test_rate_model.py
+++ rate_model/test_rate_model.py
@@ -8,75 +8,7 @@ import numpy.testing as npt
 import rate_model as rm
 import sqlite_routines
 import pytest
-
-params_dict_stationary = {
-    # main params
-    'sim_time': 10.,
-    'dt': 0.001,
-    'sampl_dt': 0.01,
-    'N': 90,
-    
-    # connectivity params
-    'J0': -10.0,
-    'J1': 16.0,
-    'J_EI': 0.0,
-    'J_IE': 0.0,
-    'eps': 0.5,
-    'conn_width': 1.0,
-    'conn_type' : 'cos',
-    'seed': 0,
-    
-    # actvity params
-    'U': 0.05,
-    'I0': 10.0,
-    'tau_d': 0.1,
-    'tau_f': 1.,
-    'tau': 0.01,
-    'alpha': 1.5,
-}
-
-params_dict_burst = {
-    # main params
-    'sim_time': 10.,
-    'dt': 0.0005,
-    'sampl_dt': 0.0005,
-    'N': 90,
-    
-    'J0': -1.*2*np.pi,
-    'J1': 12.*2*np.pi,
-    'J_EI': 1.9,
-    'J_IE': 1.8*2*np.pi,
-    'eps': 0.5,
-    'conn_width': 1/2.2,
-    'conn_type' : 'trunc_cos',
-    'seed': 0,
-    
-    'U': 0.3,
-    'I0': -0.1,
-    'tau_d': 0.3,
-    'tau_f': 4.0,
-    'tau': 0.01,
-    'alpha': 1.5,
-}
-
-
-stim_stationary = {
-    'stim_start': [.0],
-    'stim_duration': [.05],
-    'stim_ampl': [5.0],
-    'stim_pos': [0.0],
-    'stim_width': [1.],
-    'stim_type': ['cos']
-}
-
-stim_burst = {
-    'stim_start': [.0],
-    'stim_duration': [.05],
-    'stim_ampl': [390.0],
-    'stim_pos': [0.0],
-    'stim_width': [1/2.2],
-    'stim_type': ['trunc_cos']
-}
+from config import *
 
 def simulate(params, stim_params, backend='python'):
     rate_network = rm.RateNetwork.init_all_params(**params)
